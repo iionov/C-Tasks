c91824e3edb70d6e27d75ccd004e455a982d7ce6
iionov <48625130+iionov@users.noreply.github.com>
2019-03-28 23:21:01 +0300

    Add files via upload

---
+++ lang_task.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include <iostream>
+#include "c_lang_task.h"
+using std::cout;
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	short col = 3;
+	short row = 2;
+	int N = 3 , i = 4, max_sz = 20;
+	char* out_str;
+	out_str = (char*)malloc(max_sz*(sizeof(char)));
+	const char *full_fname = "test1.txt";
+	system("pause");
+	return 0;
+}
